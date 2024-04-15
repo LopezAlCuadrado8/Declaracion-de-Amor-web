fea980216e7730cca678896c4c3ecb034393f940
Daniel Lopez <Daniel Lopez>
2024-04-15 11:26:55 -0500

    Arreglo css

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ btnSi.addEventListener('click', function () {
     alert('Sabia que ivas a que decir que si uwu')
 });
 
-// Crea un evento de click para el elemento "btnNo", si es presionado se mueve aleatoriamente hacia un punto de la pantalla
-btnNo.addEventListener('click', function () {
-    const randomX = parseInt(Math.random() * 200); //Elige un numero aleatorio entre 0 y 1
-    const randomY = parseInt(Math.random() * 200);
+// Crea un evento de mouseover para el elemento "btnNo", si es presionado se mueve aleatoriamente hacia un punto de la pantalla
+btnNo.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random() * 100); //Elige un numero aleatorio entre 0 y 1
+    const randomY = parseInt(Math.random() * 100);
+    btnNo.style.setProperty('top', randomY + '%');
+    btnNo.style.setProperty('left', randomX + '%');
     btnNo.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`);
 });
